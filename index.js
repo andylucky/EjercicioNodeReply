b285f44f2240094735dac4ec8ef99456af08ccf3
andylucky <3726817+andylucky@users.noreply.github.com>
2025-02-19 12:40:47 +0000

    Nueva clase Product.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 import { User } from "./User.js";
+import { Product } from "./Product.js";
+
 const Usuario = new User("Juan", 25);
 console.log(Usuario.name);
+
+const unProducto = new Product("Cola de cacao", 10);
+console.log(unProducto.name);
